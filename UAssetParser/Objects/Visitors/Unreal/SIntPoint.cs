ebdccb5e12d3b1d283bc80c3a075ef7976d05e9f
Kein <kein-of@yandex.ru>
2019-08-12 21:04:54 +0300

    IntPoint support

---
+++ UAssetParser/Objects/Visitors/Unreal/SIntPoint.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace UAssetParser.Objects.Visitors.Unreal
+{
+    [DataContract, Description("IntPoint"), Category("Unreal")]
+    public class SIntPoint : IStructObject
+    {
+        [DataMember]
+        public int X { get; set; }
+
+        [DataMember]
+        public int Y { get; set; }
+
+        public object Serialize()
+        {
+            return this;
+        }
+    }
+}
