rinne-grid/UAssetParser

commit 7201ab8e5a1181a6ebbf7d608ef6ff6729b569da

[Serialization] add isArray switch for serializing
[Properties] add MapProperty

UObject/Properties/AbstractGuidProperty.cs
@@ -7,7 +7,7 @@ using UObject.Generics;
 namespace UObject.Properties
 {
     [PublicAPI]
-    public class AbstractGuidProperty : AbstractProperty
+    public abstract class AbstractGuidProperty : AbstractProperty
     {
         [JsonIgnore]
         public PropertyGuid Guid => (Tag as PropertyGuidTag)?.Guid ?? new PropertyGuid();
@@ -15,7 +15,7 @@ namespace UObject.Properties
         [JsonIgnore]
         public override PropertyTag? Tag { get; set; }
 
-        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor)
+        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool isArray)
         {
             Tag = new PropertyGuidTag();
             Tag.Deserialize(buffer, asset, ref cursor);

UObject/Generics/IPropertyObject.cs
@@ -1,0 +1,12 @@
+﻿using System;
+using JetBrains.Annotations;
+using UObject.Asset;
+
+namespace UObject.Generics
+{
+    [PublicAPI]
+    public interface IPropertyObject : ISerializableObject
+    {
+        void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool isArray);
+    }
+}

UObject/Generics/ISerializableObject.cs
@@ -10,10 +10,4 @@ namespace UObject.Generics
         void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor);
         void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor);
     }
-
-    [PublicAPI]
-    public interface IPropertyObject : ISerializableObject
-    {
-        void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool ignoreTag);
-    }
 }

UObject/ObjectSerializer.cs
@@ -23,6 +23,7 @@ namespace UObject
             { nameof(NameProperty), typeof(NameProperty) },
             { nameof(StrProperty), typeof(StrProperty) },
             { nameof(ArrayProperty), typeof(ArrayProperty) },
+            { nameof(MapProperty), typeof(MapProperty) },
             { nameof(EnumProperty), typeof(EnumProperty) },
             { nameof(IntProperty), typeof(IntProperty) },
             { nameof(UInt32Property), typeof(UInt32Property) },
@@ -70,7 +71,7 @@ namespace UObject
             return DeserializeProperty(buffer, asset, tag, tag.Type, tmp, ref cursor, false);
         }
 
-        public static AbstractProperty DeserializeProperty(Span<byte> buffer, AssetFile asset, PropertyTag tag, Name serializationType, int offset, ref int cursor, bool ignore)
+        public static AbstractProperty DeserializeProperty(Span<byte> buffer, AssetFile asset, PropertyTag tag, Name serializationType, int offset, ref int cursor, bool isArray)
         {
             if (serializationType == null) throw new InvalidDataException();
             if (!PropertyTypes.TryGetValue(serializationType, out var propertyType))
@@ -80,7 +81,7 @@ namespace UObject
             }
 
             if (!(Activator.CreateInstance(propertyType) is AbstractProperty instance)) throw new InvalidDataException();
-            instance.Deserialize(buffer, asset, ref cursor, ignore);
+            instance.Deserialize(buffer, asset, ref cursor, isArray);
             return instance;
         }
 

UObject/Properties/AbstractProperty.cs
@@ -7,7 +7,7 @@ using UObject.Generics;
 namespace UObject.Properties
 {
     [PublicAPI]
-    public class AbstractProperty : IPropertyObject
+    public abstract class AbstractProperty : IPropertyObject
     {
         [JsonIgnore]
         public virtual PropertyTag? Tag { get; set; }
@@ -20,10 +20,16 @@ namespace UObject.Properties
 
         public virtual void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor) => Tag?.Serialize(ref buffer, asset, ref cursor);
 
-        public virtual void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool ignoreTag)
+        public virtual void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool isArray)
         {
-            if (ignoreTag) return;
+            if (isArray) return;
             Deserialize(buffer, asset, ref cursor);
+        }
+
+        public virtual void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor, bool isArray)
+        {
+            if (isArray) return;
+            Serialize(ref buffer, asset, ref cursor);
         }
     }
 }

UObject/Properties/ArrayProperty.cs
@@ -17,23 +17,34 @@ namespace UObject.Properties
         [JsonIgnore]
         public PropertyGuid Guid { get; set; } = new PropertyGuid();
 
-        public List<object?> Values { get; set; } = new List<object?>();
+        public List<object?> Value { get; set; } = new List<object?>();
 
-        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool ignore)
+        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool isArray)
         {
-            base.Deserialize(buffer, asset, ref cursor, ignore);
+            Logger.Assert(isArray == false, "isArray == false");
+            base.Deserialize(buffer, asset, ref cursor, isArray);
             ArrayType.Deserialize(buffer, asset, ref cursor);
             Guid.Deserialize(buffer, asset, ref cursor);
             var count = SpanHelper.ReadLittleInt(buffer, ref cursor);
             if (ArrayType == "StructProperty")
             {
                 var structProperty = ObjectSerializer.DeserializeProperty(buffer, asset, Tag ?? new PropertyTag(), ArrayType, cursor, ref cursor, true) as StructProperty;
-                for (var i = 0; i < count; ++i) Values.Add(ObjectSerializer.DeserializeStruct(buffer, asset, structProperty?.StructName ?? "None", ref cursor));
+                for (var i = 0; i < count; ++i) Value.Add(ObjectSerializer.DeserializeStruct(buffer, asset, structProperty?.StructName ?? "None", ref cursor));
             }
             else
             {
-                for (var i = 0; i < count; ++i) Values.Add(ObjectSerializer.DeserializeProperty(buffer, asset, Tag ?? new PropertyTag(), ArrayType, cursor, ref cursor, true));
+                for (var i = 0; i < count; ++i) Value.Add(ObjectSerializer.DeserializeProperty(buffer, asset, Tag ?? new PropertyTag(), ArrayType, cursor, ref cursor, true));
             }
+        }
+
+        public override void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor, bool isArray)
+        {
+            Logger.Assert(isArray == false, "isArray == false");
+            base.Serialize(ref buffer, asset, ref cursor, isArray);
+            ArrayType.Serialize(ref buffer, asset, ref cursor);
+            Guid.Serialize(ref buffer, asset, ref cursor);
+            SpanHelper.WriteLittleInt(ref buffer, Value.Count, ref cursor);
+            // TODO: Serialize struct data
         }
     }
 }

UObject/Properties/BoolProperty.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text.Json.Serialization;
 using DragonLib.IO;
 using JetBrains.Annotations;
@@ -15,18 +16,20 @@ namespace UObject.Properties
         [JsonIgnore]
         public PropertyGuid Guid { get; set; } = new PropertyGuid();
 
-        public override void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor)
+        public override string ToString() => Value.ToString(CultureInfo.InvariantCulture);
+
+        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool isArray)
         {
-            base.Serialize(ref buffer, asset, ref cursor);
-            SpanHelper.WriteByte(ref buffer, (byte) (Value ? 1 : 0), ref cursor);
-            Guid.Serialize(ref buffer, asset, ref cursor);
+            base.Deserialize(buffer, asset, ref cursor, isArray);
+            Value = SpanHelper.ReadByte(buffer, ref cursor) == 1;
+            if (!isArray) Guid.Deserialize(buffer, asset, ref cursor);
         }
 
-        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool ignoreTag)
+        public override void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor, bool isArray)
         {
-            base.Deserialize(buffer, asset, ref cursor, ignoreTag);
-            Value = SpanHelper.ReadByte(buffer, ref cursor) == 1;
-            Guid.Deserialize(buffer, asset, ref cursor);
+            base.Serialize(ref buffer, asset, ref cursor, isArray);
+            SpanHelper.WriteByte(ref buffer, (byte) (Value ? 1 : 0), ref cursor);
+            if (!isArray) Guid.Serialize(ref buffer, asset, ref cursor);
         }
     }
 }

UObject/Properties/EnumProperty.cs
@@ -16,26 +16,29 @@ namespace UObject.Properties
 
         public Name Value { get; set; } = new Name();
 
-        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool ignoreTag)
+        public override string ToString() => Value;
+
+        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool isArray)
         {
-            base.Deserialize(buffer, asset, ref cursor, ignoreTag);
-            if (!ignoreTag)
+            base.Deserialize(buffer, asset, ref cursor, isArray);
+            if (!isArray)
             {
                 EnumName.Deserialize(buffer, asset, ref cursor);
                 Guid.Deserialize(buffer, asset, ref cursor);
-                Value.Deserialize(buffer, asset, ref cursor);
             }
-            else
-            {
-                Value.Deserialize(buffer, asset, ref cursor);
-            }
+
+            Value.Deserialize(buffer, asset, ref cursor);
         }
 
-        public override void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor)
+        public override void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor, bool isArray)
         {
-            base.Serialize(ref buffer, asset, ref cursor);
-            EnumName.Serialize(ref buffer, asset, ref cursor);
-            Guid.Serialize(ref buffer, asset, ref cursor);
+            base.Serialize(ref buffer, asset, ref cursor, isArray);
+            if (!isArray)
+            {
+                EnumName.Serialize(ref buffer, asset, ref cursor);
+                Guid.Serialize(ref buffer, asset, ref cursor);
+            }
+
             Value.Serialize(ref buffer, asset, ref cursor);
         }
     }

commit c96dfb5588607c6f2f9992b8e0a2d362a63307e7

[AbstractGuidProperty] fix bug introduced in 7201ab8e5a1181a6ebbf7d608ef6ff6729b569da

UObject/Properties/AbstractGuidProperty.cs
@@ -15,7 +15,7 @@ namespace UObject.Properties
         [JsonIgnore]
         public override PropertyTag? Tag { get; set; }
 
-        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, bool isArray)
+        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor)
         {
             Tag = new PropertyGuidTag();
             Tag.Deserialize(buffer, asset, ref cursor);