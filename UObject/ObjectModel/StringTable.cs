a3114547a66255367818247cdf7a8e572c98a579
Kein <kein-of@yandex.ru>
2020-05-04 04:41:27 +0300

    StringTable support

--- UObject/ObjectModel/StringTable.cs
+++ UObject/ObjectModel/StringTable.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using DragonLib.IO;
 using JetBrains.Annotations;
 using UObject.Asset;
+using UObject.Generics;
 
 namespace UObject.ObjectModel
 {
     [PublicAPI]
-    public class StringTable : UnrealObject
+    public class StringTable : ISerializableObject
     {
-        public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor) => throw new NotImplementedException();
+        public string Name { get; set; } = "None";
+        public int Reserved { get; set; }
+        public Dictionary<string, string> Data { get; set; } = new Dictionary<string, string>();
+        public UnrealObject ExportData { get; set; } = new UnrealObject();
 
-        public override void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor) => throw new NotImplementedException();
+        public void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor)
+        {
+            ExportData.Deserialize(buffer, asset, ref cursor);
+            Reserved = SpanHelper.ReadLittleInt(buffer, ref cursor);
+            Name = ObjectSerializer.DeserializeString(buffer, ref cursor);
+            var count = SpanHelper.ReadLittleInt(buffer, ref cursor);
+            for(int i = 0; i < count; ++i)
+                Data.Add(ObjectSerializer.DeserializeString(buffer, ref cursor),
+                         ObjectSerializer.DeserializeString(buffer, ref cursor));
+        }
+
+        public void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor) => throw new NotImplementedException();
     }
 }
