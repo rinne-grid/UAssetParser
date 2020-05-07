54d2db4e31e29a0399e99f9c95ec403ca892bd2f
Kein <kein-of@yandex.ru>
2020-05-07 14:41:17 +0300

    Rear TextProperty properly, dependng on flags. Ugly, but works

--- UObject/Properties/TextProperty.cs
+++ UObject/Properties/TextProperty.cs
@@ -12,14 +12,12 @@ namespace UObject.Properties
     [PublicAPI]
     public class TextProperty : AbstractGuidProperty, IValueType<string?>
     {
-#if DEBUG
-        public static HashSet<int> Unknown3Set = new HashSet<int>();
-#endif
-        public int SerializationFlags { get; set; }
-        public byte Flags { get; set; }
-        public int Unknown3 { get; set; }
+        public int LocalizeFlag { get; set; }
+        public byte KeyPresent { get; set; }
+        public int TextPresent { get; set; }
         public PropertyGuid ValueGuid { get; set; } = new PropertyGuid();
-        public string? Hash { get; set; }
+        public string? Namespace { get; set; }
+        public string? Hashkey { get; set; }
         public string? Value { get; set; }
 
         public override string? ToString() => Value;
@@ -27,37 +25,42 @@ namespace UObject.Properties
         public override void Deserialize(Span<byte> buffer, AssetFile asset, ref int cursor, SerializationMode mode)
         {
             base.Deserialize(buffer, asset, ref cursor, mode);
-            SerializationFlags = SpanHelper.ReadLittleInt(buffer, ref cursor);
-            Logger.Assert(SerializationFlags == 0 || SerializationFlags == 8, "SerializationFlags == 0 || SerializationFlags == 8", SerializationFlags.ToString());
-            // Unknown3 and Flags are combined as an int32. There's an excess zero guid.
-            Flags = SpanHelper.ReadByte(buffer, ref cursor);
-            if (mode != SerializationMode.Normal || Tag?.Size > 5)
+            LocalizeFlag = SpanHelper.ReadLittleInt(buffer, ref cursor);
+            KeyPresent = SpanHelper.ReadByte(buffer, ref cursor);
+            TextPresent = SpanHelper.ReadLittleInt(buffer, ref cursor);
+            // TODO: 4.18 serialized uasset with TextProperty in array/struct
+            if (Tag?.Size > 5 || mode != SerializationMode.Normal)
             {
-                Unknown3 = SpanHelper.ReadLittleInt(buffer, ref cursor);
-#if DEBUG
-                Unknown3Set.Add(Unknown3);
-#endif
-                if ((SerializationFlags & 8) == 8) ValueGuid.Deserialize(buffer, asset, ref cursor);
-                if (Flags != 0xFF)
+                if (KeyPresent == 0)
                 {
-                    Hash = ObjectSerializer.DeserializeString(buffer, ref cursor);
-                    Value = ObjectSerializer.DeserializeString(buffer, ref cursor);
+                    if (TextPresent < 0 || TextPresent > 1)
+                    {
+                        cursor -= 4;
+                        Namespace = ObjectSerializer.DeserializeString(buffer, ref cursor);
+                    }
+                    if (TextPresent == 1)
+                        ValueGuid.Deserialize(buffer, asset, ref cursor);
+                    Hashkey = ObjectSerializer.DeserializeString(buffer, ref cursor);
                 }
+                if (TextPresent == 1 || KeyPresent == 0)
+                    Value = ObjectSerializer.DeserializeString(buffer, ref cursor);
             }
         }
 
+        // TODO - redo for serialization. Do we update flags after reading back from JSON
+        // or do we just write flags based on the state of the values (key, namespace, textvalue)?
         public override void Serialize(ref Memory<byte> buffer, AssetFile asset, ref int cursor, SerializationMode mode)
         {
             base.Serialize(ref buffer, asset, ref cursor, mode);
-            SpanHelper.WriteLittleInt(ref buffer, SerializationFlags, ref cursor);
-            SpanHelper.WriteByte(ref buffer, Flags, ref cursor);
-            if (mode != SerializationMode.Normal || Tag?.Size > 5)
+            SpanHelper.WriteLittleInt(ref buffer, LocalizeFlag, ref cursor);
+            SpanHelper.WriteByte(ref buffer, KeyPresent, ref cursor);
+            if (Tag?.Size > 5 || mode != SerializationMode.Normal)
             {
-                SpanHelper.WriteLittleInt(ref buffer, Unknown3, ref cursor);
-                if ((SerializationFlags & 8) == 8) ValueGuid.Serialize(ref buffer, asset, ref cursor);
-                if (Flags != 0xFF)
+                SpanHelper.WriteLittleInt(ref buffer, TextPresent, ref cursor);
+                if ((LocalizeFlag & 8) == 8) ValueGuid.Serialize(ref buffer, asset, ref cursor);
+                if (KeyPresent != 0xFF)
                 {
-                    ObjectSerializer.SerializeString(ref buffer, Hash ?? string.Empty, ref cursor);
+                    ObjectSerializer.SerializeString(ref buffer, Hashkey ?? string.Empty, ref cursor);
                     ObjectSerializer.SerializeString(ref buffer, Value ?? string.Empty, ref cursor);
                 }
             }
